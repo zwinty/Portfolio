c6396cd8f4ea08f975e0e33d517d0f8d0c2980d8
zwinty <zinwintyee@gmail.com>
2025-04-28 15:19:31 +0900

    spelling-correct

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import FirstView from './components/FristView/FirstView';
 import Navigation from './components/Navigation/Navigation';
-import Project from './components/Project/project';
+import Project from './components/Project/Project';
 
 function App() {
   return (
